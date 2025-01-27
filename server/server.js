19348edce67099a1cae8bc81e5a8737e560979e0
Eran <eran.hasarali.2001@gmail.com>
2025-01-27 21:31:25 +0530

    autosave off

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const db = require('./config/db.js');
-const usersRoutes = require('./routes/usersRoutes.js')
-const cors = require('cors');
+const express = require("express");
+const db = require("./config/db.js");
+const usersRoutes = require("./routes/usersRoutes.js");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ urlencoded: false }));
 
-app.get('/api', usersRoutes);
+app.get("/api", usersRoutes);
+
+console.log("Hello");
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
